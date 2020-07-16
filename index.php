a6b68a6826c869ace6d3a9f1bc05a28419388da8
hendurhance <50846992+hendurhance@users.noreply.github.com>
2020-07-16 14:18:07 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sociolify</title>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/app.css">
+    <!-- Custom JS -->
+    <script src="assets/js/app.js"></script>
+    <!-- Bootstrap Framework-->
+    <link rel="stylesheet" href="libs/css/bootstrap.min.css">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="libs/css/fontawesome-all.min.css">
+</head>
+<body>
+    
+   
+<!-- NAVIGATION -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-mine static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img class="logo" src="assets/img/sociolify.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-align-right hamburger"></i>
+        </button>
+       <div class="collapse navbar-collapse" id="navbarResponsive">
+         <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><i class="fas fa-home"></i>   Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i>   Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fas fa-user"></i>   Registration</a>
+            </li>
+         </ul>
+       </div>
+      </div>
+    </nav>
+
+<!-- HEADER -->
+  <section class="header-img">
+  <div class="container">
+     <div class="row">
+       <div class="col-lg-6 col sm-12 col-md-6">
+         <h1 class="header-h1">
+           The best marketing doesn't feel like silly marketing!
+         </h1>
+         <p class="header-p">
+           Marketing is telling the world you're a rockstar
+         </p>
+         <button class="btn btn-cta">
+         <i class="fas fa-rocket"></i> Get Started
+         </button>
+       </div>
+     </div>
+   </div>
+  </section>
+
+<!-- OUTLINE SECTION -->
+  <section class="outline-sec">
+    <div class="container-fluid outline-container">
+     <div class="row no-gutters">
+      <div class="col-lg-3">
+        <div class="outline-div out-1">
+         <i class="fas fa-search outline-icon"></i>
+         <h2>
+           Search Engine Optimization
+         </h2>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="outline-div out-2">
+        <i class="far fa-comment-alt outline-icon"></i>
+         <h2>
+           Social Media Marketing
+         </h2>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="outline-div out-3">
+         <i class="far fa-envelope-open outline-icon"></i>
+         <h2>
+           Email List Marketing
+         </h2>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="outline-div out-4">
+         <i class="far fa-file-alt outline-icon"></i>
+         <h2>
+           Content Development
+         </h2>
+        </div>
+      </div>
+     </div>
+    </div>
+  </section>
+
+<!-- ABOUT -->
+   <section class="about-section">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-6 col-md-6 col-sm-12">
+           <div class="about-img">
+             <img src="assets/img/marketing.svg" class="img-fluid align-items-center" alt="">
+           </div>
+         </div>
+         <div class="col-lg-6 col-md-6 col-sm-12">
+             <div class="about-content align-items-center">
+               <h3>Crafting Out A Niche For Your Business</h3>
+               <p>Because advertising and marketing is an art, the solution
+               to each new problem challenge should begin with a blank canvas and
+               open mind, not with nervous borrowings of other people's mediocrities precisely modern.</p>
+             </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+
+<!-- SERVICES -->
+   <section class="service-section">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="service-container">
+            <div class="service-main">
+              <div class="service-content">
+                <h4>
+                  Product Design & Web Development
+                </h4>
+                <img src="assets/img/service_img3.png" alt="">
+                <div class="dash"></div>
+                <p>PRODUCT DESIGN & WEB DEVELOPMENT/MANAGEMENT</p>
+              </div>
+            </div>
+          </div>
+       </div>
+       <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="service-container">
+            <div class="service-main">
+              <div class="service-content">
+                <h4>
+                  Market Analysis & Product Growth
+                </h4>
+                <img src="assets/img/service_img1.png" alt="">
+                <div class="dash"></div>
+                <p>STUDY THE MARKET AND HELP IMPROVE PRODUCT SALE</p>
+              </div>
+            </div>
+          </div>
+       </div>
+       <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="service-container">
+            <div class="service-main">
+              <div class="service-content">
+                <h4>
+                  SEO & Digital Campaign
+                </h4>
+                <img src="assets/img/service_img4.png" alt="">
+                <div class="dash"></div>
+                <p>SEARCH ENGINE OPTIMIZATION AND DIGITAL CAMPAIGNS</p>
+              </div>
+            </div>
+          </div>
+       </div>
+       <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="service-container">
+            <div class="service-main">
+              <div class="service-content">
+                <h4>
+                  Branding & Branding Strategy
+                </h4>
+                <img src="assets/img/service_img2.png" alt="">
+                <div class="dash"></div>
+                <p>PRODUCT BRANDING AND BRANDING STRATEGY</p>
+              </div>
+            </div>
+          </div>
+       </div>
+       </div>
+     </div>
+   </section>
+
+<!-- WHY US --> 
+  <section class="why-us">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-6 col-md-6 col-sm-12">
+         <div class="why-us-img">
+           <img src="assets/img/why-us.png" class="img-fluid" alt="">
+         </div>
+       </div>
+       <div class="col-lg-6 col-md-6 col-sm-12">
+         <div class="why-us-content">
+           <h3>
+            Taking Your Business To The Next Level
+           </h3>
+           <p>
+             We are passionate about our work. Our designers,
+             marketers, developers stay ahead of the curve to
+             provide engaging and user-friendly services to
+             make your business stand out. We care about your
+             business, which is why we work with you.
+           </p>
+         </div>
+       </div>
+     </div>
+   </div>
+  </section>
+
+
+
+<!-- TESTIMONIALS -->
+   <section class="testimonials">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-6 col-md-12 col-sm-12">
+           <div class="testimonial-content">
+             <h3>
+              What Client Say About Us
+             </h3>
+             <p>
+             Sociolify clients and their results
+             are the best proof that our
+             methodologies work.
+             But we’ve also received
+             numerous awards and praise
+             from the media and our peers.
+             </p>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 col-sm-12">
+           <div class="testimonial-container">
+             <div class="testimonial-section">
+               <h4>
+                 MARK JOCKEY
+               </h4>
+               <p>
+                 Happiness is not something
+                 you postpone for the future;
+                 it is something you design
+                 for the present. If you don't
+                 design your own life plan.
+               </p>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 col-sm-12">
+           <div class="testimonial-container">
+             <div class="testimonial-section">
+               <h4>
+                 MARK JOCKEY
+               </h4>
+               <p>
+                 Happiness is not something
+                 you postpone for the future;
+                 it is something you design
+                 for the present. If you don't
+                 design your own life plan.
+               </p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+<!-- BLOG -->
+   <section class="blog">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-4 col-md-6 col-sm-12">
+           <div class="blog-container">
+             <div class="blog-section">
+               <div class="blog-img">
+                 <img src="assets/img/blog-1.jpg" class="img-fluid" alt="">
+               </div>
+               <p> July 13, 2020 </p>
+               <h4>
+                  The Basics of Branding
+               </h4>
+               <a href="#">Read More <i class="fas fa-long-arrow-alt-right"></i></a>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+           <div class="blog-container">
+             <div class="blog-section">
+               <div class="blog-img">
+                 <img src="assets/img/blog-2.jpg" class="img-fluid" alt="">
+               </div>
+               <p> July 13, 2020 </p>
+               <h4>
+                  How to Increase your CPC?
+               </h4>
+               <a href="#">Read More <i class="fas fa-long-arrow-alt-right"></i></a>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-12">
+           <div class="blog-container">
+             <div class="blog-section">
+               <div class="blog-img">
+                <img src="assets/img/blog-3.jpg" class="img-fluid" alt="">
+               </div>
+               <p> July 13, 2020 </p>
+               <h4>
+                 Search Engine Optimization
+               </h4>
+               <a href="#">Read More <i class="fas fa-long-arrow-alt-right"></i></a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+<!-- CONTACT -->
+    <section class="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="contact-section">
+              <i class="far fa-envelope-open-text"></i>
+              <p>+2349091646111</p>
+              <p>hendurhance@outlook.com</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="contact-section">
+              <i class="far fa-map-marker-alt"></i>
+              <p>99 Radicula Street, Lekki, Lagos, Nigeria.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-12 col-sm-12">
+            <div class="contact-section">
+              <input placeholder="Email Address..." type="text">
+              <input type="button" value="SUBSCRIBE">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+</body>
+    <!-- jQuery -->
+    <script src="libs/js/jquery.slim.min.js"></script>
+    <!-- Bootstrap Framework -->
+    <script src="libs/js/bootstrap.bundle.min.js"></script>
+    
+</html>
